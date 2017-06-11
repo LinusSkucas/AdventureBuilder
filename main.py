8d0bd27ff40890c8a967e3313c51a57da7880f34
Linus Skucas <linus33@comcast.net>
2017-06-10 20:53:57 -0700

    Made drop function in section

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 def road2():
 	print("On road 2")
+	
+def getshovel():
+	print("got shovel")
+
+def usestuff():
+	print("Used stuff")
 
 class section():
-    "A section of the text based game"
-    def __init__(self, name, description,getable, useable, directions):
-        self.name = name
-        self.longName = description
-        self.getable = list(getable)
-        self.useable = list(useable)
-        self.directions = dict(directions)
+	"A section of the text based game"
+	def __init__(self, name, description, getable, useable, directions):
+		self.name = name
+		self.longName = description
+		self.getable = dict(getable)
+		self.useable = dict(useable)
+		self.directions = dict(directions)
 
-    def look(self):
-        return self.longName
+	def look(self):
+		print(self.longName)
+		
+	def drop(self, tool):
+		if tool not in self.useable.keys():
+			print("You Can't use that.")
+		else:
+			#print("Using {}..." % tool)
+			toolfunc = self.useable[tool]
+			toolfunc()
+				
 
-road = section("Road", "Long road", getable=["shovel", "hoe"], useable=["stuff"], directions={"w": road2})
-print(road.look)
+road = section("Road", "Long road", getable={"shovel":getshovel}, useable={"stuff":usestuff}, directions={"w": road2})
+road.look()
+road.drop("junk")
